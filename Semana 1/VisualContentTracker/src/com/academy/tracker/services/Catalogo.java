35a96deb8a2ee699a6163f02c87185746cb60c43
User <lizzyinzunza934@gmail.com>
2026-08-14 16:27:14 -0600

    feat: agrega clase Catalogo con ArrayList para gestionar coleccion de contenidos

---
+++ Semana 1/VisualContentTracker/src/com/academy/tracker/services/Catalogo.java
@@ -0,0 +1,37 @@
+package com.academy.tracker.services;
+
+import com.academy.tracker.models.ContenidoVisual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalogo {
+    //declarar lista dinamica
+    //verificar que acepta cualquier contenido visual (series, futuras peliculas) por polimofismo
+    private List<ContenidoVisual> listaContenidos;
+
+    //Contructor, inicializar la lista vacia cuando nace el catalogo
+    public Catalogo() {
+        this.listaContenidos = new ArrayList<>();
+    }
+
+    //guardar cosas en la lista - metodo
+    public void agregarContenido(ContenidoVisual contenido){
+        listaContenidos.add(contenido);
+        System.out.println("Registro exitoso: '" + contenido.getTitulo() + "' se añadió al catálogo.");
+    }
+
+    //imprimir todo usando ciclo for-each (imperativo) - metodo
+    public void mostrarTodo() {
+        System.out.println("\n=== MI CATALÓGO DE CONTENIDO VISUAL ===");
+        if (listaContenidos.isEmpty()) {
+            System.out.println("El catáloo está vacío.");
+        } else {
+            //cada ContenidoVisual 'c' que exista dentro de 'listaContenidos' ...
+            for (ContenidoVisual c : listaContenidos) {
+                c.mostrarDetalles();
+            }
+        }
+        System.out.println("Total histórico de reistros: " + ContenidoVisual.getTotalRegistros());
+    }
+}
