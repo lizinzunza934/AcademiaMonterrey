c0a5813ffbc24b1e5cc27cd8aa0e60cd9c0fbe89
User <lizzyinzunza934@gmail.com>
2026-08-14 13:07:09 -0600

    test: implementa ejecucion en Main para validar instanciacion y polimorfismo de SerieAnimada

--- Semana 1/VisualContentTracker/src/Main.java
+++ Semana 1/VisualContentTracker/src/Main.java
@@ -1,15 +1,35 @@
+import com.academy.tracker.models.SerieAnimada;
+
+import java.nio.file.FileSystemNotFoundException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello  and welcome!");
+        System.out.println("=== INICIANDO VISUAL TRACKER ===");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //llamar nuestro molde SerieAnimada
+        SerieAnimada miSerie = new SerieAnimada("Jujutsu Kaisen", 24);
+
+        //ver que se heredó el metodo de mostrar detalles y titulos
+        miSerie.mostrarDetalles();
+
+        //usar el comportamiento de una serie
+        miSerie.incrementarCapitulo();
+        miSerie.incrementarCapitulo();
+
+        //probar el contrato de la interfaz (actualizar la calificación)
+        System.out.print("\n--- Calificando la serie ---");
+        miSerie.actualizarCalificacion(9.5); //probar con número valido
+        miSerie.actualizarCalificacion(15.0); //forzar error para verificar si la validacion funciona
+
+        //ver detalles actualizados
+        miSerie.mostrarDetalles();
+
+        //probar polimorfismo
+        System.out.print("\n--- Reproduciendo ---");
+        miSerie.reproducir();
         }
     }
-}
