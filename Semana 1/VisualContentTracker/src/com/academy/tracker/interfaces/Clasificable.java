bed191eb024252cb5d2afd83decec70b368b08a9
User <lizzyinzunza934@gmail.com>
2026-08-14 00:44:01 -0600

    feat: agrega interfaz Clasificable para estandarizar calificaciones

--- Semana 1/VisualContentTracker/src/com/academy/tracker/interfaces/Clasificable.java
+++ Semana 1/VisualContentTracker/src/com/academy/tracker/interfaces/Clasificable.java
@@ -1,5 +1,5 @@
 package com.academy.tracker.interfaces;
-
+//Flexibilidad para sistemas que escalen a futuro
 public interface Clasificable {
     void actualizarCalificacion(double nuevaCalificacion);
 
